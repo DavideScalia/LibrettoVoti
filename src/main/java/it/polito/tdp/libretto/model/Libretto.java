6ef6c43861ecc008232bd9e4395f106b0dd895d1
Davide Scalia <s178325@studenti.polito.it>
2023-07-04 23:20:55 +0200

    Punti 1 e 2

--- src/main/java/it/polito/tdp/libretto/model/Libretto.java
+++ src/main/java/it/polito/tdp/libretto/model/Libretto.java
@@ -1,6 +1,49 @@
 package it.polito.tdp.libretto.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Libretto {
 
-	//nuovo
+	// Gestirà un elenco di voti
+	// farà quindi una lista di voti
+	private List<Voto> voti;
+
+	public Libretto() {
+		this.voti = new ArrayList<Voto>();
+
+	}
+
+	/**
+	 * Aggiungi un nuovo voto al libretto (per ora non fa nessun controllo)
+	 * 
+	 * @param v il Voto da aggiungere
+	 * @return true
+	 */
+	// creo un metodo add per il caricamneto degli esami
+	public boolean add(Voto v) {
+		return this.voti.add(v);
+	}
+
+	public void stampa() {
+		for (Voto v : this.voti) {
+			System.out.println(v);
+		}
+	}
+
+	//Metodo1
+	//esami con voto = a 25
+	public void stampa25 () {
+		for (Voto v : this.voti) {
+			if (v.getPunti() == 25)
+				System.out.println(v);
+		}
+	}
+	//metodo 2 stampa punti uguale ad un certo valore
+	public void stampaPuntiUguali (int valore) {
+		for (Voto v : this.voti) {
+			if (v.getPunti() == 25)
+				System.out.println(v);
+		}
+	}
 }
--- src/main/java/it/polito/tdp/libretto/model/TestLibretto.java
+++ src/main/java/it/polito/tdp/libretto/model/TestLibretto.java
@@ -1,10 +1,27 @@
 package it.polito.tdp.libretto.model;
 
+import java.time.LocalDate;
+
 public class TestLibretto {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Libretto lib = new Libretto() ;
+		
+		lib.add(new Voto("Analisi 1", 29, LocalDate.of(2021, 2, 15))) ;
+		lib.add(new Voto("Fisica 2", 21, LocalDate.of(2022, 6, 30))) ;
+		lib.add(new Voto("Fisica 1", 25, LocalDate.of(2023, 6, 30))) ;
+	
+		//Per stampare tutti i voti 
+		//lib.stampa();
+		//per stampare i voti = 25
+		//lib.stampa25();
+		
+		
+		//stampa voti con valore uguale al parametro che dichiaro 
+		lib.stampaPuntiUguali(25);
 	}
-
+	
+	
 }
